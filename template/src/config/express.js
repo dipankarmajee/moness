7a4144b2318a30f5acc486a213748f9ba36b745a
Dipankar Majee <dipankar.majee@simplyfi.tech>
2023-09-18 21:42:19 +0530

    file structure changes

--- template/src/config/express.js
+++ template/src/config/express.js
@@ -7,6 +7,10 @@ const app = express();
 // Middleware
 app.use(morgan("dev"));
 app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api/users", require("../routes/userRoutes"));
